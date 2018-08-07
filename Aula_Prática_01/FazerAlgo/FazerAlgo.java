f95f550b232d5bb27171eddeca61caf901cdfcb1
Lucas Gabriel Matias Paiva <lucasgmpaiva@A305-24L.imd.local>
2018-08-07 07:43:21 -0300

    Primeiro Exercício

---
+++ Aula_Prática_01/FazerAlgo/FazerAlgo.java
@@ -0,0 +1,12 @@
+public class FazerAlgo{
+	public FazerAlgo(){}
+
+	public void fazer(){
+		System.out.println("Feito! =D");
+	}
+
+	public String fazer(int x){
+		String retorno="OK: " + x;
+		return retorno;	
+	}
+}
